f387c294dd3257bfc8be1ac12e01f87401c58b2d
Chris Ball <chris@echobind.com>
2019-07-19 16:51:39 -0400

    MVP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `👋 💻 💬 Welcome to Dev Chat! 💬 💻 👋`,
+    description: `A place for all developers to chat about all the things.`,
+    author: `@cball`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +8,39 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div
+      style={{
+        marginTop: 80,
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Image />
+      <p style={{ fontSize: 28, marginBottom: 8, marginTop: 24 }}>
+        A place for all developers to chat about all the things.
+      </p>
+      <p style={{ fontSize: 18 }}>Dev, Hobbies, Randomness, and Life!</p>
+
+      <p style={{ fontSize: 20, fontWeight: "bold" }}>
+        The chat is over. Stay tuned for the next one.
+      </p>
+      {/* <a
+        href="https://zoom.us/j/564989895"
+        style={{
+          background: "#ffb961",
+          color: "#fff",
+          padding: "16px",
+          borderRadius: 4,
+          textDecoration: "none",
+          margin: "20px auto  ",
+          display: "block",
+          width: 200,
+        }}
+      >
+        Join the Video Chat!
+      </a> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
